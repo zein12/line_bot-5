c89557f192f4ee9c00546306b75c021896e4f1a3
hero.hsu <shesheshe98@hotmail.com>
2016-05-13 17:38:48 +0800

    line bot first.

---
+++ README.md
@@ -0,0 +1,12 @@
+LineApplication
+LineApplication BOT API
+
+必須條件
+
+1. Line channels 平台帳號 (不定時開放申請)
+
+2. 有支援https且開443port的server
+    #懶的用https? 可以用 https://www.webscript.io 撰寫lua程式打到個人server
+    #免費版 webscript 只能放七天, 但測試夠用了
+
+3. 創意!創意!創意!
---
+++ callback.php
@@ -0,0 +1,135 @@
+<?php
+
+class LineApplication {
+
+    protected $keyword = [
+        // HandleCommand
+        'betago' => 'BetaGoCommand',
+        // GoogleCommand
+        'google' => 'GoogleCommand'
+    ];
+    
+    public function callback() {
+    
+        $input = json_decode(file_get_contents('php://input'), TRUE);
+        
+        if(empty($input) && $input['content']['contentType'] != '1') {
+            
+            $output = $this->handle($input);
+            
+        } else {
+            $command = explode(' ', $input['content']['text']);
+            
+            if(isset($this->keyword[$command[0]])) {
+                
+                $handle = new $this->keyword[$command[0]];
+                
+            } else {
+                
+                $handle = new HelpCommand;
+                
+            }
+            
+            $output = $handle->handle($command);
+        }
+        
+        $this->output($output);
+    }
+    
+    function getContentType($value) {
+        $type_code = array(
+            1 => "Text message",
+            2 => "Image message",
+            3 => "Video message",
+            4 => "Video message",
+            7 => "Location message",
+            8 => "Sticker message",
+            10 => "Contact message"
+        );
+        
+        if(array_key_exists($value, $type_code)) {
+            return $type_code[$value];
+        }
+        
+        return "unknown";
+    }
+
+    public function output($message) {
+        echo json_encode(array('msg'=>$message));
+    }
+    public function handle($command) {
+        return 'no service';
+    }
+    
+}
+
+class BetaGoCommand extends LineApplication{
+    public function handle($command){
+    
+        $point_map = [
+            ["┌","─","┬","─","┬","─","┬","─","┬","─","┐"],
+            ["├","─","┼","─","┼","─","┼","─","┼","─","┤"],
+            ["├","─","┼","─","┼","─","┼","─","┼","─","┤"],
+            ["├","─","┼","─","┼","─","┼","─","┼","─","┤"],
+            ["├","─","┼","─","┼","─","┼","─","┼","─","┤"],
+            ["└","─","┴","─","┴","─","┴","─","┴","─","┘"]
+        ];
+
+        $x = $command[1];
+        $x = $x < 1 ? 1 : $x;
+        $x = $x > 4 ? 4 : $x;
+        
+        $y = $command[2];
+        $y = $y < 1 ? 1 : $y;
+        $y = $y > 4 ? 4 : $y;
+        $y = $y * 2;
+        
+        $pc_x = array_diff([1, 2, 3, 4], [$x]);
+        sort($pc_x);
+        $pc_y = array_diff([2, 4, 6, 8], [$y]);
+        sort($pc_y);
+        
+        $pc_x = $pc_x[rand(0, count($pc_x)-1)];
+        $pc_y = $pc_y[rand(0, count($pc_y)-1)];
+        
+        $point_map[$x][$y] = "●";
+        $point_map[$pc_x][$pc_y] = "○";
+        
+        $str = "BetaGo v0.0.0.1\r\n";
+        $str .= "your is black, coordinate is (" . $command[1] . ", " . $command[2] . ").\r\n";
+        $str .= "pc is white, coordinate is (" . $pc_x . ", ". ($pc_y/2) . ").\r\n";
+
+        foreach($point_map as $row) {
+            foreach($row as $col) {
+                $str .= $col;
+            }
+            $str .= "\r\n";
+        }
+        
+        return $str;
+    }
+}
+class GoogleCommand extends LineApplication {
+    public function handle($command) {
+        return 'https://www.google.com.tw/?#q='.urlencode($command[1]);
+    }
+}
+
+class HelpCommand extends LineApplication {
+    public function handle($command) {
+        $message = [
+            '有事嗎?',
+            '有病嗎?',
+            '幹嘛拉',
+            '無聊喔',
+            '....',
+            '你來亂的阿!!',
+            'www',
+            '( ’?`)y??~~'
+        ];
+        return $message[rand(0, 7)];
+    }
+}
+
+$bot = new LineApplication;
+$bot->callback();
